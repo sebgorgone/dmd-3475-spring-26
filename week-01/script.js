3e5c290ab4a0f4a7127532e52f781ee64b2b6fea
sebby-omarchy <sebgorgone@gmail.com>
2026-01-23 21:18:27 -0500

    name change in week 1 script.js

--- week-01/script.js
+++ week-01/script.js
@@ -4,5 +4,4 @@ console.log("Week 1 loaded");
 
 // TODO (Day 1):
 // Change this message so it includes your name.
-let message = "Hello, my name is [YOUR NAME HERE]";
-console.log(message);
+let message = "Hello, my name is Sebastian"
