d2c69e905e0e469bb0e65b4e85a7f7f0af08dfef
dinomolasses <sebgorgone>
2026-01-29 11:51:11 -0500

    week 2 thursday

---
+++ week-02/script.js
@@ -0,0 +1,31 @@
+var week = 2;
+
+console.log('this is week 2');
+
+//alert('hello this is week 2');
+
+var today = 'thursday';
+var tomorrow = 'friday';
+
+//console.log(today)
+
+//console.log('today is thurday');
+//console.log('tomorrow is friday');
+
+var greeting = 'Today is ' + today + '.';
+
+console.log(greeting);
+
+//console.log(`today is ${today}`);
+//console.log(`tomorrow is ${tomorrow}`);
+
+var nextWeek = week + 1
+console.log('next week will be week ' + nextWeek);
+
+
+//var name = 'joel';
+
+
+var name = prompt('what is your name');
+
+alert('my name is ' + name);
